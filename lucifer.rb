babf336e3fa65f7b74d003f826e508887d6e6094
Alexandre Magno Teles Zimerer <alexmagno@corp.globo.com>
2010-07-30 19:03:53 -0300

    teste para obter pergunta do usuario

--- lucifer.rb
+++ lucifer.rb
@@ -1,8 +1,8 @@
 
 class Lucifer
+  
   def obter_resposta(n)
     respostas = File.open('respostas.txt', 'r')
-    return respostas.readlines[n].strip
-    respostas.close  
+    return respostas.readlines[n].strip  
   end
 end
--- lucifer_spec.rb
+++ lucifer_spec.rb
@@ -15,6 +15,7 @@ end
 describe Lucifer, "Imprimindo resposta para usuarios que nao ativaram o modo resposta" do
   before do
     @lucifer = Lucifer.new
+    allow_message_expectations_on_nil
   end
   it "deve trazer uma resposta do tipo string" do
      @lucifer.obter_resposta(1).class.should == String
@@ -38,4 +39,30 @@ describe Lucifer, "Imprimindo resposta para usuarios que nao ativaram o modo res
         @lucifer.obter_resposta(1).should == 'Vai procurar outra coisa para fazer otário!'
    end
    
+   it "deve retornar nil quando e passada um indice que nao existe ou outro tipo" do
+      mock_file = mock('File')
+      stub_respostas = ['Tira a mão suja deste teclado!\n','Vai procurar outra coisa para fazer otário!\n'] 
+      stub_respostas[2].should_receive(:strip).and_return(nil)
+      mock_file.should_receive(:readlines).and_return(stub_respostas)
+      File.should_receive(:open).with('respostas.txt','r').and_return(mock_file)
+      @lucifer.obter_resposta(2).should be_nil
+   end
+   
 end
+
+describe Lucifer, "recebendo a pergunta e devolvendo a resposta" do
+  
+  before do
+    @lucifer = Lucifer.new
+    allow_message_expectations_on_nil
+  end  
+  
+  it "deve receber o input e gerar uma resposta" do
+    pergunta = "Com quantos paus se faz uma canoa?"
+    resposta = @lucifer.perguntar(pergunta)
+    
+    resposta.should == "Tira a mão suja deste teclado"
+    
+  end
+  
+end   
