6d363c09af9eeb2205004de182d5293902b7f210
Alexandre Magno Teles Zimerer <alexmagno@corp.globo.com>
2010-08-03 19:14:17 -0300

    criado o executavel do programa

---
+++ main.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+require "lucifer"
+
+sessao = Lucifer.new
+puts "Faça sua pergunta: "
+pergunta = gets.chomp
+resposta = sessao.perguntar(pergunta)
+puts "Resposta: " + resposta
